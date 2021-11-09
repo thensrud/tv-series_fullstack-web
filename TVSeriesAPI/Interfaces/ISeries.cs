e1af2b2d444b64ba89c245a3fcff7df51d4c44f1
thensrud <thensrud@hotmail.com>
2021-11-09 23:34:20 +0100

    Works without interface :construction:

--- TVSeriesAPI/Interfaces/ISeries.cs
+++ TVSeriesAPI/Interfaces/ISeries.cs
@@ -1,20 +1,21 @@
-using System.Collections.Generic;
+// using System.Collections.Generic;
 
-namespace TVSeriesAPI.Interfaces
-{
-    public interface ISeries
-    {
-        string Id { get; set; }
-        string Name { get; set; }
-        string Image { get; set; }
-        string Genre { get; set; }
-        string Plot { get; set; }
-        List<ISeasons> Seasons { get; set; }
-    }
+// namespace TVSeriesAPI.Interfaces
+// {
+//   public interface ISeries
+//   {
+//     string Id { get; set; }
+//     string Name { get; set; }
+//     string Image { get; set; }
+//     string Genre { get; set; }
+//     string Plot { get; set; }
+//     List<ISeasons> Seasons { get; set; }
+//   }
 
-    public interface ISeasons
-    {
-        string SeasonNumber { get; set; }
-        List<string> Episodes { get; set; }
-    }
-}
+//   public interface ISeasons
+//   {
+//     // egen fil?
+//     string SeasonNumber { get; set; }
+//     List<string> Episodes { get; set; }
+//   }
+// }
--- TVSeriesAPI/Models/Series.cs
+++ TVSeriesAPI/Models/Series.cs
@@ -1,27 +1,26 @@
 using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using TVSeriesAPI.Interfaces;
+// using TVSeriesAPI.Interfaces;
 
 namespace TVSeriesAPI.Models 
 {
-    public class Series : ISeries
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Image { get; set; }
-        public string Genre { get; set; }
-        public string Plot { get; set; }
-        public List<ISeasons> Seasons { get; set; }
-    }
+    // bruker ikke interface nå
+  public class Series //: ISeasons
+  {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Image { get; set; }
+    public string Genre { get; set; }
+    public string Plot { get; set; }
+    public List<Seasons> Seasons { get; set; }
+  }
 
-    public class Seasons : ISeasons
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string SeasonNumber { get; set; }
-        public List<string> Episodes { get; set; }
-    }
+  public class Seasons
+  {
+    public string SeasonNumber { get; set; }
+    public List<string> Episodes { get; set; }
+  }
 }
