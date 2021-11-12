8e869e0e02983a352222e399a47ea970a0a5d65d
thensrud <thensrud@hotmail.com>
2021-11-12 15:42:04 +0100

    Expanded the episode class and interface

--- TVSeriesAPI/Interfaces/IEpisode.cs
+++ TVSeriesAPI/Interfaces/IEpisode.cs
@@ -3,5 +3,7 @@ namespace TVSeriesAPI.Interfaces
     public interface IEpisode
     {
         string Name { get; set; }
+        string SeasonNumber { get; set; }
+        string EpisodeNumber { get; set; }
     }
 }
--- TVSeriesAPI/Models/Episode.cs
+++ TVSeriesAPI/Models/Episode.cs
@@ -5,5 +5,7 @@ namespace TVSeriesAPI.Models
     public class Episode : IEpisode
     {
       public string Name { get; set; }
+      public string SeasonNumber { get; set; }
+      public string EpisodeNumber { get; set; }
     }
 }
