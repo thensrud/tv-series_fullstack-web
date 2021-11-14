c1dcfe2c9785bbc9aaa7cbda6483f39aee0ee16c
Waligustav <wgbjork@gmail.com>
2021-11-14 23:40:47 +0100

    trying to fetch correct image from seriesContext (failing!)

--- tvseries/src/components/actors/ActorsDetails.tsx
+++ tvseries/src/components/actors/ActorsDetails.tsx
@@ -3,34 +3,61 @@ import { FC, useContext, useEffect, useState } from 'react';
 import { ActorsContext } from '../../contexts/ActorsContext';
 import { ActorsContextType } from '../../types/ActorsContextType';
 import { IActors } from '../../interfaces/IActors';
+import { IInSeries } from '../../interfaces/IInSeries';
+
+import { SeriesContext } from '../../contexts/SeriesContext';
+import { SeriesContextType } from '../../types/SeriesContextType';
+import { ISeries } from '../../interfaces/ISeries';
 
 const ActorsDetails: FC = () => {
   const { id } = useParams();
 
   const { getActorsById } = useContext(ActorsContext) as ActorsContextType;
+  const { getSeriesById } = useContext(SeriesContext) as SeriesContextType;
   // Trenger egentlig ikke state her, kan vaere statisk objekt
   const [actor, setActor] = useState<IActors>();
+  const [series, setSeries] = useState<ISeries | any>();
 
   useEffect(() => {
     if (id) {
       const _actor = getActorsById(id);
+      const _serie = getSeriesById(id);
       setActor(_actor);
+      setSeries(_serie);
     }
   }, []);
 
+  const matchSeries = (_name: string) => {
+    return series?.find((serie: { name: string }) => serie.name === _name);
+  };
+
+  const createInSeriesList = () => {
+    return actor?.inSeries?.map((series: IInSeries, key: number) => {
+      return (
+        <div key={key}>
+          <h5>{series.name}</h5>
+          {console.log(matchSeries(series.name))}
+          <img src={`https://localhost:5001/images/$`} width='150' />
+          {
+            // Trenger logikk her for å hente ut bilde av riktig serie fra serie-collection,
+            //som også linker til den seriens side.
+          }
+        </div>
+      );
+    });
+  };
+
   return (
     <section>
-      <h3>Name:</h3>
+      <img src={`https://localhost:5001/images/${actor?.image}`} width='450' />
+      <h3>Name: {actor?.name}</h3>
       <article>
-        <h4>Age: </h4>
-        <h4>ID: {id}</h4>
+        <h4>Age: {actor?.age} </h4>
+        <h4>Nationality: {actor?.country}</h4>
+        <h4>Featured in: {createInSeriesList()}</h4>
+        <p>ID: {id}</p>
       </article>
     </section>
-    //{actor?.age} {actor?.name}
-    //Map out inSeries in individual cards -
-    //Fix in ActorItem.tsx and insert the variables below
-    //<Card.Text>Nationality: {actor?.country}</Card.Text>
-    //<Card.Text>Featured in: {actor?.inSeries}</Card.Text>
   );
 };
 
