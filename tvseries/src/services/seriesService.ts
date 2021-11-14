47653c25297087a9ddacc444533bcc0f06dff35a
Sarah van Dijk <54410259+Sarahcvd@users.noreply.github.com>
2021-11-14 18:11:15 +0100

    Controllers (#7)
    * Poorly attempt at hiding imagename in backend
    
    * added exception handling to services

--- tvseries/src/services/seriesService.ts
+++ tvseries/src/services/seriesService.ts
@@ -1,28 +1,44 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { ISeries } from '../interfaces/ISeries';
 
 export const seriesService = (function () {
-	const urlToSeriesController = 'https://localhost:5001/series';
-	const urlToImageUploadController =
-		'https://localhost:5001/ImageUpload/SaveImage';
+  const urlToSeriesController = 'https://localhost:5001/series';
+  const urlToImageUploadController =
+    'https://localhost:5001/ImageUpload/SaveImage';
 
-	const getAllSeries = async () => {
-		const result = await axios.get(urlToSeriesController);
-		return result.data as ISeries[];
-	};
+  const getAllSeries = async () => {
+    try {
+      const result = await axios.get(urlToSeriesController);
+      return result.data as ISeries[];
+    } catch (error) {
+      const err = error as AxiosError;
+      if (err.response) {
+        console.log(err.response.status);
+        console.log(err.response.data);
+      }
+    }
+  };
 
-	const postSeries = async (newSeries: ISeries, image: File) => {
-		let formData = new FormData();
-		formData.append('file', image);
+  const postSeries = async (newSeries: ISeries, image: File) => {
+    let formData = new FormData();
+    formData.append('file', image);
 
-		axios.post(urlToSeriesController, newSeries);
-		axios({
-			url: urlToImageUploadController,
-			method: 'POST',
-			data: formData,
-			headers: { 'Content-Type': 'multipart/form-data' },
-		});
-	};
+    try {
+      axios.post(urlToSeriesController, newSeries);
+      axios({
+        url: urlToImageUploadController,
+        method: 'POST',
+        data: formData,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+    } catch (error) {
+      const err = error as AxiosError;
+      if (err.response) {
+        console.log(err.response.status);
+        console.log(err.response.data);
+      }
+    }
+  };
 
-	return { getAllSeries, postSeries };
+  return { getAllSeries, postSeries };
 })();
