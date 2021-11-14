fb6486960a51365ca3dd4e0fad3bb52c6250b81f
Waligustav <wgbjork@gmail.com>
2021-11-14 19:56:04 +0100

    Editing navigation links and removing non-functioning exceptionhandling

--- tvseries/src/services/actorsService.ts
+++ tvseries/src/services/actorsService.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { IActors } from '../interfaces/IActors';
 
 export const actorsService = (function () {
@@ -8,37 +8,21 @@ export const actorsService = (function () {
     'https://localhost:5001/ImageUpload/SaveImage';
 
   const getAllActors = async () => {
-    try {
-      const result = await axios.get(urlToActorsController);
-      return result.data as IActors[];
-    } catch (error) {
-      const err = error as AxiosError;
-      if (err.response) {
-        console.log(err.response.status);
-        console.log(err.response.data);
-      }
-    }
+    const result = await axios.get(urlToActorsController);
+    return result.data as IActors[];
   };
 
   const postActors = async (newActors: IActors, image: File) => {
     let formData = new FormData();
     formData.append('file', image);
 
-    try {
-      axios.post(urlToActorsController, newActors);
-      axios({
-        url: urlToImageUploadController,
-        method: 'POST',
-        data: formData,
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-    } catch (error) {
-      const err = error as AxiosError;
-      if (err.response) {
-        console.log(err.response.status);
-        console.log(err.response.data);
-      }
-    }
+    axios.post(urlToActorsController, newActors);
+    axios({
+      url: urlToImageUploadController,
+      method: 'POST',
+      data: formData,
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
   };
 
   return { getAllActors, postActors };
--- tvseries/src/services/seriesService.ts
+++ tvseries/src/services/seriesService.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { ISeries } from '../interfaces/ISeries';
 
 export const seriesService = (function () {
@@ -7,37 +7,21 @@ export const seriesService = (function () {
     'https://localhost:5001/ImageUpload/SaveImage';
 
   const getAllSeries = async () => {
-    try {
-      const result = await axios.get(urlToSeriesController);
-      return result.data as ISeries[];
-    } catch (error) {
-      const err = error as AxiosError;
-      if (err.response) {
-        console.log(err.response.status);
-        console.log(err.response.data);
-      }
-    }
+    const result = await axios.get(urlToSeriesController);
+    return result.data as ISeries[];
   };
 
   const postSeries = async (newSeries: ISeries, image: File) => {
     let formData = new FormData();
     formData.append('file', image);
 
-    try {
-      axios.post(urlToSeriesController, newSeries);
-      axios({
-        url: urlToImageUploadController,
-        method: 'POST',
-        data: formData,
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-    } catch (error) {
-      const err = error as AxiosError;
-      if (err.response) {
-        console.log(err.response.status);
-        console.log(err.response.data);
-      }
-    }
+    axios.post(urlToSeriesController, newSeries);
+    axios({
+      url: urlToImageUploadController,
+      method: 'POST',
+      data: formData,
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
   };
 
   return { getAllSeries, postSeries };
