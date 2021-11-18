3feddd77038d4617f3a4acec3764d4d616c9b325
thensrud <thensrud@hotmail.com>
2021-11-18 14:25:54 +0100

    Buttons now working in list-pages

--- tvseries/src/components/actors/ActorItem.tsx
+++ tvseries/src/components/actors/ActorItem.tsx
@@ -1,43 +1,56 @@
-import { FC, useContext } from "react";
-import { Button, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { ActorsContext } from "../../contexts/ActorsContext";
-import { IActors } from "../../interfaces/IActors";
-import { ActorsContextType } from "../../types/ActorsContextType";
+import { FC, useContext } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { To, useNavigate } from 'react-router-dom';
+import { ActorsContext } from '../../contexts/ActorsContext';
+import { IActors } from '../../interfaces/IActors';
+import { ActorsContextType } from '../../types/ActorsContextType';
 
 const ActorItem: FC<IActors> = ({
-  id,
-  name,
-  image,
-  age,
-  country,
-  inSeries,
+	id,
+	name,
+	image,
+	age,
+	country,
+	inSeries,
 }) => {
-  const { deleteActor } = useContext(ActorsContext) as ActorsContextType;
+	const { deleteActor } = useContext(ActorsContext) as ActorsContextType;
 
-  const removeActor = () => {
-    if (id) {
-      deleteActor(id);
-    }
-  };
+	const navigate = useNavigate();
+	const handleClick = (path: To) => {
+		navigate(path);
+	};
 
-  return (
-    <Card>
-      <Card.Img
-        variant="top"
-        src={`https://localhost:5001/images/${image}`}
-        alt={name}
-      />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Link to={`/actors-details/${id}`}>Read more about {name}</Link>
-      </Card.Body>
+	const removeActor = () => {
+		if (id) {
+			deleteActor(id);
+		}
+	};
 
-      <Button variant="danger" onClick={removeActor}>
-        Delete
-      </Button>
-    </Card>
-  );
+	return (
+		<Card>
+			<Card.Img
+				variant="top"
+				src={`https://localhost:5001/images/${image}`}
+				alt={name}
+			/>
+			<Card.Body>
+				<Card.Title>{name}</Card.Title>
+			</Card.Body>
+
+			<div className="d-grid m-1 gap-1">
+				<Button
+					variant="primary"
+					onClick={() => handleClick(`/actors-details/${id}`)}
+				>
+					Read more
+				</Button>
+
+				<Button variant="outline-danger" onClick={removeActor}>
+					Delete
+				</Button>
+			</div>
+		</Card>
+	);
 };
 
 export default ActorItem;
--- tvseries/src/components/series/SeriesItem.tsx
+++ tvseries/src/components/series/SeriesItem.tsx
@@ -1,37 +1,51 @@
-import { FC, useContext } from "react";
-import { Button, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { SeriesContext } from "../../contexts/SeriesContext";
-import { ISeries } from "../../interfaces/ISeries";
-import { SeriesContextType } from "../../types/SeriesContextType";
+import { FC, useContext } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { To, useNavigate } from 'react-router-dom';
+import { SeriesContext } from '../../contexts/SeriesContext';
+import { ISeries } from '../../interfaces/ISeries';
+import { SeriesContextType } from '../../types/SeriesContextType';
 
 const SeriesItem: FC<ISeries> = ({ id, name, image, plot, genres }) => {
-  const { deleteSeries } = useContext(SeriesContext) as SeriesContextType;
+	const { deleteSeries } = useContext(SeriesContext) as SeriesContextType;
 
-  const removeSeries = () => {
-    if (id) {
-      deleteSeries(id);
-    }
-  };
+	const navigate = useNavigate();
+	const handleClick = (path: To) => {
+		navigate(path);
+	};
 
-  return (
-    <Card>
-      <Card.Img
-        variant="top"
-        src={`https://localhost:5001/images/${image}`}
-        alt={name}
-      />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Subtitle className="pb-3">{genres} </Card.Subtitle>
-        <Card.Text>{plot}</Card.Text>
-        <Link to={`/series-details/${id}`}>Read more about {name}</Link>
-      </Card.Body>
-      <Button variant="danger" onClick={removeSeries}>
-        Delete
-      </Button>
-    </Card>
-  );
+	const removeSeries = () => {
+		if (id) {
+			deleteSeries(id);
+		}
+	};
+
+	return (
+		<Card>
+			<Card.Img
+				variant="top"
+				src={`https://localhost:5001/images/${image}`}
+				alt={name}
+			/>
+			<Card.Body>
+				<Card.Title>{name}</Card.Title>
+				<Card.Subtitle className="pb-3">{genres} </Card.Subtitle>
+				<Card.Text>{plot}</Card.Text>
+			</Card.Body>
+
+			<div className="d-grid m-1 gap-1">
+				<Button
+					variant="primary"
+					onClick={() => handleClick(`/series-details/${id}`)}
+				>
+					Read more
+				</Button>
+
+				<Button variant="outline-danger" onClick={removeSeries}>
+					Delete
+				</Button>
+			</div>
+		</Card>
+	);
 };
 
 export default SeriesItem;
