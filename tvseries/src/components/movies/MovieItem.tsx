ade3915b64f0ab5a19ff6cc87ab61a2c509af6d1
Waligustav <wgbjork@gmail.com>
2021-11-20 16:23:14 +0100

    Merge branch 'master' of https://github.com/thensrud/tv-series_fullstack-web into master

--- tvseries/src/components/movies/MovieItem.tsx
+++ tvseries/src/components/movies/MovieItem.tsx
@@ -6,45 +6,52 @@ import { IMovies } from '../../interfaces/IMovies';
 import { MoviesContextType } from '../../types/MoviesContextType';
 
 const MovieItem: FC<IMovies> = ({ id, name, image, plot, genres }) => {
-	const { deleteMovies } = useContext(MoviesContext) as MoviesContextType;
+  const { deleteMovies } = useContext(MoviesContext) as MoviesContextType;
 
-	const navigate = useNavigate();
-	const handleClick = (path: To) => {
-		navigate(path);
-	};
+  const navigate = useNavigate();
+  const handleClick = (path: To) => {
+    navigate(path);
+  };
 
-	const removeMovie = () => {
-		if (id) {
-			deleteMovies(id);
-		}
-	};
+  const removeMovie = () => {
+    let confirmResult: boolean = window.confirm(
+      `Are you sure you want to delete ${name} from the database?`
+    );
+    if (id && confirmResult) {
+      try {
+        deleteMovies(id);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-	return (
-		<Card>
-			<Card.Img
-				style={{ height: '250px' }}
-				variant="top"
-				src={`https://localhost:5001/images/${image}`}
-				alt={name}
-			/>
-			<Card.Body>
-				<Card.Title>{name}</Card.Title>
-				<Card.Subtitle className="pb-3">{genres}</Card.Subtitle>
-				<Card.Text>{plot}</Card.Text>
-			</Card.Body>
-			<div className="d-grid m-1 gap-1">
-				<Button
-					variant="primary"
-					onClick={() => handleClick(`/movies-details/${id}`)}
-				>
-					Read more
-				</Button>
-				<Button variant="outline-danger" onClick={removeMovie}>
-					Delete
-				</Button>
-			</div>
-		</Card>
-	);
+  return (
+    <Card>
+      <Card.Img
+        style={{ height: '250px' }}
+        variant='top'
+        src={`https://localhost:5001/images/${image}`}
+        alt={name}
+      />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Subtitle className='pb-3'>{genres}</Card.Subtitle>
+        <Card.Text>{plot}</Card.Text>
+      </Card.Body>
+      <div className='d-grid m-1 gap-1'>
+        <Button
+          variant='primary'
+          onClick={() => handleClick(`/movies-details/${id}`)}
+        >
+          Read more
+        </Button>
+        <Button variant='outline-danger' onClick={removeMovie}>
+          Delete
+        </Button>
+      </div>
+    </Card>
+  );
 };
 
 export default MovieItem;
--- tvseries/src/components/series/SeriesItem.tsx
+++ tvseries/src/components/series/SeriesItem.tsx
@@ -6,47 +6,54 @@ import { ISeries } from '../../interfaces/ISeries';
 import { SeriesContextType } from '../../types/SeriesContextType';
 
 const SeriesItem: FC<ISeries> = ({ id, name, image, plot, genre }) => {
-	const { deleteSeries } = useContext(SeriesContext) as SeriesContextType;
+  const { deleteSeries } = useContext(SeriesContext) as SeriesContextType;
 
-	const navigate = useNavigate();
-	const handleClick = (path: To) => {
-		navigate(path);
-	};
+  const navigate = useNavigate();
+  const handleClick = (path: To) => {
+    navigate(path);
+  };
 
-	const removeSeries = () => {
-		if (id) {
-			deleteSeries(id);
-		}
-	};
+  const removeSeries = () => {
+    let confirmResult: boolean = window.confirm(
+      `Are you sure you want to delete ${name} from the database?`
+    );
+    if (id && confirmResult) {
+      try {
+        deleteSeries(id);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-	return (
-		<Card>
-			<Card.Img
-				style={{ height: '250px' }}
-				variant="top"
-				src={`https://localhost:5001/images/${image}`}
-				alt={name}
-			/>
-			<Card.Body>
-				<Card.Title>{name}</Card.Title>
-				{/* <Card.Subtitle className="pb-3">{genre} </Card.Subtitle> */}
-				<Card.Text>{plot}</Card.Text>
-			</Card.Body>
+  return (
+    <Card>
+      <Card.Img
+        style={{ height: '250px' }}
+        variant='top'
+        src={`https://localhost:5001/images/${image}`}
+        alt={name}
+      />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        {/* <Card.Subtitle className="pb-3">{genre} </Card.Subtitle> */}
+        <Card.Text>{plot}</Card.Text>
+      </Card.Body>
 
-			<div className="d-grid m-1 gap-1">
-				<Button
-					variant="primary"
-					onClick={() => handleClick(`/series-details/${id}`)}
-				>
-					Read more
-				</Button>
+      <div className='d-grid m-1 gap-1'>
+        <Button
+          variant='primary'
+          onClick={() => handleClick(`/series-details/${id}`)}
+        >
+          Read more
+        </Button>
 
-				<Button variant="outline-danger" onClick={removeSeries}>
-					Delete
-				</Button>
-			</div>
-		</Card>
-	);
+        <Button variant='outline-danger' onClick={removeSeries}>
+          Delete
+        </Button>
+      </div>
+    </Card>
+  );
 };
 
 export default SeriesItem;
