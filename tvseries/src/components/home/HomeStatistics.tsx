25e0a388b7e3920e913102b6212587c1eb3b3fd6
thensrud <thensrud@hotmail.com>
2021-11-20 11:34:57 +0100

    Simple statistics on home page

--- tvseries/src/components/home/HomeStatistics.tsx
+++ tvseries/src/components/home/HomeStatistics.tsx
@@ -1,22 +1,27 @@
 import { FC, useContext } from 'react';
+import { Alert } from 'react-bootstrap';
 import { ActorsContext } from '../../contexts/ActorsContext';
+import { MoviesContext } from '../../contexts/MoviesContext';
 import { SeriesContext } from '../../contexts/SeriesContext';
 import { ActorsContextType } from '../../types/ActorsContextType';
+import { MoviesContextType } from '../../types/MoviesContextType';
 import { SeriesContextType } from '../../types/SeriesContextType';
 
 const HomeStatistics: FC = () => {
 	const { actors } = useContext(ActorsContext) as ActorsContextType;
 	const { series } = useContext(SeriesContext) as SeriesContextType;
+	const { movies } = useContext(MoviesContext) as MoviesContextType;
 
 	const nOfActors = actors.length;
 	const nOfSeries = series.length;
+	const nOfMovies = movies.length;
 
 	return (
-		<>
-			<p>
-				Number of actors: {nOfActors} and number of series: {nOfSeries}
-			</p>
-		</>
+		<div className="mt-4">
+			<Alert variant="primary">Number of actors: {nOfActors}</Alert>
+			<Alert variant="info">Number of series: {nOfSeries}</Alert>
+			<Alert variant="secondary">Number of movies: {nOfMovies}</Alert>
+		</div>
 	);
 };
 
--- tvseries/src/pages/HomePage.tsx
+++ tvseries/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
+import HomeStatistics from '../components/home/HomeStatistics';
 
 const HomePage: FC = () => {
 	return (
 		<section className="section">
 			<h3 className="page-title mb-2">Home</h3>
+			<HomeStatistics />
 		</section>
 	);
 };
