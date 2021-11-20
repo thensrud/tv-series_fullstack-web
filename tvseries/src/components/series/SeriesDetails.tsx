514725c415c66a3bfc271c4123356acc4a70e97e
Sarah van Dijk <sarahmurstad@hotmail.com>
2021-11-20 23:36:26 +0100

    Bugfix

--- tvseries/src/components/series/SeriesDetails.tsx
+++ tvseries/src/components/series/SeriesDetails.tsx
@@ -1,15 +1,15 @@
-import { useParams } from 'react-router';
-import { FC, useContext, useEffect, useState } from 'react';
-import { SeriesContext } from '../../contexts/SeriesContext';
-import { SeriesContextType } from '../../types/SeriesContextType';
-import { ISeries } from '../../interfaces/ISeries';
-import { ActorsContext } from '../../contexts/ActorsContext';
-import { ActorsContextType } from '../../types/ActorsContextType';
-import { To, useNavigate } from 'react-router-dom';
-import { Button, Card, Col, Row } from 'react-bootstrap';
-import { IEpisode } from '../../interfaces/IEpisode';
-import { IInSeries } from '../../interfaces/IInSeries';
-import { IGenre } from '../../interfaces/IGenre';
+import { useParams } from "react-router";
+import { FC, useContext, useEffect, useState } from "react";
+import { SeriesContext } from "../../contexts/SeriesContext";
+import { SeriesContextType } from "../../types/SeriesContextType";
+import { ISeries } from "../../interfaces/ISeries";
+import { ActorsContext } from "../../contexts/ActorsContext";
+import { ActorsContextType } from "../../types/ActorsContextType";
+import { To, useNavigate } from "react-router-dom";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { IEpisode } from "../../interfaces/IEpisode";
+import { IInSeries } from "../../interfaces/IInSeries";
+import { IGenre } from "../../interfaces/IGenre";
 
 const SeriesDetails: FC = () => {
   const { id } = useParams();
@@ -40,24 +40,22 @@ const SeriesDetails: FC = () => {
       // Mapping through all series an actor has featured in
       return featuringIn?.map((sName: IInSeries, key: number) => {
         if (sName.name.toLowerCase() === series?.name.toLowerCase()) {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           amountOfFeaturedActors += 1;
           return (
-            <Col className='mt-1' sm={6} md={4} lg={3} xl={2} key={key}>
-              <Card className='mini-card'>
+            <Col className="mt-1" sm={6} md={4} lg={3} xl={2} key={key}>
+              <Card className="mini-card">
                 <Card.Img
-                  variant='top'
+                  variant="top"
                   src={`https://localhost:5001/images/${actor?.image}`}
                   alt={actor?.name}
                 />
                 <Card.Body>
                   <Card.Title>{actor.name}</Card.Title>
-                  <Card.Subtitle>
-                    Has starred in {amountOfFeaturedActors} series
-                  </Card.Subtitle>
                 </Card.Body>
 
                 <Button
-                  variant='primary'
+                  variant="primary"
                   onClick={() => handleClick(`/actors-details/${actor.id}`)}
                 >
                   Read more
@@ -90,24 +88,24 @@ const SeriesDetails: FC = () => {
 
   return (
     <section>
-      <h2 className='mb-3'>{series?.name}</h2>
+      <h2 className="mb-3">{series?.name}</h2>
       <img
         src={`https://localhost:5001/images/${series?.image}`}
-        style={{ height: '350px' }}
+        style={{ height: "350px" }}
         alt={series?.name}
       />
-      <h4 className='mb-3 mt-4'>Plot:</h4>
+      <h4 className="mb-3 mt-4">Plot:</h4>
       <p>{series?.plot}</p>
-      <h4 className='mb-3 mt-4'>Episodes:</h4>
+      <h4 className="mb-3 mt-4">Episodes:</h4>
       <div>{renderEpisodes()}</div>
       <h4>Genres: {getGenres()}</h4>
       <Button
-        className='mt-4'
+        className="mt-4"
         onClick={() => handleClick(`/edit-series/${id}`)}
       >
         Edit Series Info
       </Button>
-      <h4 className='mt-4'>Actors featured in this series:</h4>
+      <h4 className="mt-4">Actors featured in this series:</h4>
       <Row>{createInActorsList()}</Row>
     </section>
   );
