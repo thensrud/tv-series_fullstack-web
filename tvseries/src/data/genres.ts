25d56a4a6bc239d405666be80f19c895947ddb1c
thensrud <thensrud@hotmail.com>
2021-11-21 21:58:08 +0100

    Checkboxes working

---
+++ tvseries/src/data/genres.ts
@@ -0,0 +1,26 @@
+export const genres = [
+	{
+		name: 'Action',
+	},
+	{
+		name: 'Adventure',
+	},
+	{
+		name: 'Sci-Fi',
+	},
+	{
+		name: 'Comedy',
+	},
+	{
+		name: 'Romance',
+	},
+	{
+		name: 'Horror',
+	},
+	{
+		name: 'Thriller',
+	},
+	{
+		name: 'Drama',
+	},
+];
