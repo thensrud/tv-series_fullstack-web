c20329f4536c1e2be1bab18c9c8248dd5badb80a
Sarah van Dijk <sarahmurstad@hotmail.com>
2021-11-21 19:51:32 +0100

    Added page not found

---
+++ tvseries/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const NotFound: FC = () => {
+  return (
+    <section className="section">
+      <h1>404 Page Not Found</h1>
+      <p>The requestet page does not exist</p>
+    </section>
+  );
+};
+
+export default NotFound;
