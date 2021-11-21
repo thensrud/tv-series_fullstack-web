f4457f1c83c7ece47dd12fb1253a1abb70fa24ef
Sarah van Dijk <sarahmurstad@hotmail.com>
2021-11-21 22:00:57 +0100

    Updated name

--- tvseries/src/components/shared/MainNavigation.tsx
+++ tvseries/src/components/shared/MainNavigation.tsx
@@ -1,47 +1,44 @@
-import { FC } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { FC } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const MainNavigation: FC = () => {
-	return (
-		<Navbar className="px-4" bg="dark" expand="lg" variant="dark">
-			<Navbar.Brand as={Link} to="/">
-				Series Database
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="me-auto">
-					<Nav.Link as={Link} to="/">
-						Home
-					</Nav.Link>
-
-					<Nav.Link as={Link} to="all-series">
-						All series
-					</Nav.Link>
-					<Nav.Link as={Link} to="create-series">
-						Add new series
-					</Nav.Link>
-
-					<Nav.Link as={Link} to="all-actors">
-						All actors
-					</Nav.Link>
-					<Nav.Link as={Link} to="create-actor">
-						Add new actor
-					</Nav.Link>
-
-					<Nav.Link as={Link} to="all-movies">
-						All movies
-					</Nav.Link>
-					<Nav.Link as={Link} to="create-movies">
-						Add new movie
-					</Nav.Link>
-					<Nav.Link as={Link} to="trailer">
-						Trailers
-					</Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
-	);
+  return (
+    <Navbar className="px-4" bg="dark" expand="lg" variant="dark">
+      <Navbar.Brand as={Link} to="/">
+        OMG-DB
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="all-series">
+            All series
+          </Nav.Link>
+          <Nav.Link as={Link} to="create-series">
+            Add new series
+          </Nav.Link>
+          <Nav.Link as={Link} to="all-actors">
+            All actors
+          </Nav.Link>
+          <Nav.Link as={Link} to="create-actor">
+            Add new actor
+          </Nav.Link>
+          <Nav.Link as={Link} to="all-movies">
+            All movies
+          </Nav.Link>
+          <Nav.Link as={Link} to="create-movies">
+            Add new movie
+          </Nav.Link>
+          <Nav.Link as={Link} to="trailer">
+            Trailers
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default MainNavigation;
