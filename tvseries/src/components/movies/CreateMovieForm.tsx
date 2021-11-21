9d2ba724d3a5e5951f14d398960153155c802e8b
thensrud <thensrud@hotmail.com>
2021-11-21 21:58:08 +0100

    Checkboxes working

--- tvseries/src/components/movies/CreateMovieForm.tsx
+++ tvseries/src/components/movies/CreateMovieForm.tsx
@@ -1,184 +1,155 @@
-import React, { ChangeEvent, FC, useEffect, useState } from "react";
-import { Badge, Button, FloatingLabel, Form } from "react-bootstrap";
-import { IGenre } from "../../interfaces/IGenre";
-import { IMovies } from "../../interfaces/IMovies";
-import { moviesService } from "../../services/moviesService";
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import { Badge, Button, FloatingLabel, Form } from 'react-bootstrap';
+import { IGenre } from '../../interfaces/IGenre';
+import { IMovies } from '../../interfaces/IMovies';
+import { moviesService } from '../../services/moviesService';
+import { genres } from '../../data/genres';
 
 const CreateMovieForm: FC = () => {
-  const [newGenreName, setNewGenreName] = useState<string>("");
-  const [newGenre, setNewGenre] = useState<IGenre[]>([]);
-  const [newImage, setNewImage] = useState<File>();
-  const [newMovie, setNewMovie] = useState<IMovies>({
-    name: "",
-    image: "",
-    genre: newGenre,
-    plot: "",
-  });
+	const [newGenre, setNewGenre] = useState<IGenre[]>([]);
+	const [newImage, setNewImage] = useState<File>();
+	const [newMovie, setNewMovie] = useState<IMovies>({
+		name: '',
+		image: '',
+		genre: newGenre,
+		plot: '',
+	});
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    let { name, value, checked } = event.target;
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		let { name, value, checked } = event.target;
 
-    switch (name) {
-      case "movieName":
-        setNewMovie({ ...newMovie, name: value });
-        break;
-      case "image":
-        let { files } = event.target;
-        if (files) {
-          setNewMovie({ ...newMovie, image: files[0].name });
-          setNewImage(files[0]);
-        }
-        break;
-      case "Action":
-      case "Adventure":
-      case "Sci-Fi":
-      case "Comedy":
-      case "Romance":
-      case "Horror":
-      case "Thriller":
-      case "Drama":
-        if (checked) {
-          setNewGenreName(name);
-        }
-        break;
-      case "plot":
-        setNewMovie({ ...newMovie, plot: value });
-        break;
-      default:
-        break;
-    }
-  };
+		switch (name) {
+			case 'movieName':
+				setNewMovie({ ...newMovie, name: value });
+				break;
+			case 'image':
+				let { files } = event.target;
+				if (files) {
+					setNewMovie({ ...newMovie, image: files[0].name });
+					setNewImage(files[0]);
+				}
+				break;
+			case 'Action':
+			case 'Adventure':
+			case 'Sci-Fi':
+			case 'Comedy':
+			case 'Romance':
+			case 'Horror':
+			case 'Thriller':
+			case 'Drama':
+				if (checked) {
+					setNewGenre([...newGenre, { name }]);
+				} else if (!checked) {
+					const result = newGenre.filter(
+						({ name }) => name !== event.target.name
+					);
+					setNewGenre(result);
+				}
+				break;
+			case 'plot':
+				setNewMovie({ ...newMovie, plot: value });
+				break;
+			default:
+				break;
+		}
+	};
 
-  useEffect(() => {
-    setNewMovie({ ...newMovie, genre: newGenre });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newGenre]);
+	useEffect(() => {
+		setNewMovie({ ...newMovie, genre: newGenre });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [newGenre]);
 
-  const listGenres = () => {
-    return newGenre.map((genre: IGenre, key: number) => {
-      return (
-        <Badge className="mx-1" bg="secondary" key={key}>
-          {genre.name}
-        </Badge>
-      );
-    });
-  };
+	const listGenres = () => {
+		return newGenre.map((genre: IGenre, key: number) => {
+			return (
+				<Badge className="mx-1" bg="secondary" key={key}>
+					{genre.name}
+				</Badge>
+			);
+		});
+	};
 
-  const postNewMovie = () => {
-    moviesService.postMovies(newMovie, newImage as File);
+	const postNewMovie = () => {
+		moviesService.postMovies(newMovie, newImage as File);
 
-    setNewMovie({
-      name: "",
-      image: "",
-      genre: [],
-      plot: "",
-    });
-    setNewGenreName("");
-    setNewGenre([]);
-  };
+		setNewMovie({
+			name: '',
+			image: '',
+			genre: [],
+			plot: '',
+		});
+		setNewGenre([]);
+	};
 
-  useEffect(() => {
-    setNewGenre([...newGenre, { name: newGenreName }]);
-    console.log("HERE " + newGenreName);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newGenreName]);
+	const listGenresCheckbox = () => {
+		return genres.map((genre, key) => {
+			return (
+				<Form.Check
+					inline
+					label={genre.name}
+					name={genre.name}
+					type="checkbox"
+					key={key}
+					onChange={handleChange}
+				/>
+			);
+		});
+	};
 
-  const genres = [
-    {
-      name: "Action",
-    },
-    {
-      name: "Adventure",
-    },
-    {
-      name: "Sci-Fi",
-    },
-    {
-      name: "Comedy",
-    },
-    {
-      name: "Romance",
-    },
-    {
-      name: "Horror",
-    },
-    {
-      name: "Thriller",
-    },
-    {
-      name: "Drama",
-    },
-  ];
+	return (
+		<div>
+			{/* Movie name */}
+			<FloatingLabel
+				className="input-label"
+				controlId="movieName"
+				label="Movie name"
+			>
+				<Form.Control
+					onChange={handleChange}
+					name="movieName"
+					type="text"
+					placeholder="Movie name"
+					value={newMovie.name}
+				/>
+			</FloatingLabel>
 
-  const listGenresCheckbox = () => {
-    return genres.map((genre, key) => {
-      return (
-        <Form.Check
-          inline
-          label={genre.name}
-          name={genre.name}
-          type="checkbox"
-          key={key}
-          onChange={handleChange}
-        />
-      );
-    });
-  };
+			{/* Image */}
+			<Form.Group controlId="movieImage" className="mb-3 mt-3">
+				<Form.Label>Movie image</Form.Label>
+				<Form.Control onChange={handleChange} name="image" type="file" />
+			</Form.Group>
 
-  return (
-    <div>
-      {/* Movie name */}
-      <FloatingLabel
-        className="input-label"
-        controlId="movieName"
-        label="Movie name"
-      >
-        <Form.Control
-          onChange={handleChange}
-          name="movieName"
-          type="text"
-          placeholder="Movie name"
-          value={newMovie.name}
-        />
-      </FloatingLabel>
+			{/* Plot */}
+			<FloatingLabel className="input-label" controlId="plot" label="Plot">
+				<Form.Control
+					onChange={handleChange}
+					className="mb-3"
+					as="textarea"
+					style={{ height: '100px' }}
+					name="plot"
+					type="text"
+					placeholder="Once upon a time..."
+					value={newMovie.plot}
+				/>
+			</FloatingLabel>
 
-      {/* Image */}
-      <Form.Group controlId="movieImage" className="mb-3 mt-3">
-        <Form.Label>Movie image</Form.Label>
-        <Form.Control onChange={handleChange} name="image" type="file" />
-      </Form.Group>
+			{/* Genre */}
+			<div className="mb-3">{listGenresCheckbox()}</div>
 
-      {/* Plot */}
-      <FloatingLabel className="input-label" controlId="plot" label="Plot">
-        <Form.Control
-          onChange={handleChange}
-          className="mb-3"
-          as="textarea"
-          style={{ height: "100px" }}
-          name="plot"
-          type="text"
-          placeholder="Once upon a time..."
-          value={newMovie.plot}
-        />
-      </FloatingLabel>
+			{/* List all genres added */}
+			<p className="mt-1">Genres added: {listGenres()}</p>
 
-      {/* Genre */}
-      <div className="mb-3">{listGenresCheckbox()}</div>
-
-      {/* List all genres added */}
-      <p className="mt-1">Genres added: {listGenres()}</p>
-
-      {/* Post new movie */}
-      <Button
-        className="mt-3"
-        onClick={postNewMovie}
-        variant="primary"
-        type="submit"
-      >
-        Submit new movie
-      </Button>
-    </div>
-  );
+			{/* Post new movie */}
+			<Button
+				className="mt-3"
+				onClick={postNewMovie}
+				variant="primary"
+				type="submit"
+			>
+				Submit new movie
+			</Button>
+		</div>
+	);
 };
 
 export default CreateMovieForm;
--- tvseries/src/components/series/CreateSeriesForm.tsx
+++ tvseries/src/components/series/CreateSeriesForm.tsx
@@ -1,304 +1,275 @@
-import React, { FC, ChangeEvent, useState, useEffect, useContext } from "react";
-import { seriesService } from "../../services/seriesService";
-import { ISeries } from "../../interfaces/ISeries";
-import { Badge, Button, Col, FloatingLabel, Form, Row } from "react-bootstrap";
-import { IGenre } from "../../interfaces/IGenre";
-import { IEpisode } from "../../interfaces/IEpisode";
-import { SeriesContext } from "../../contexts/SeriesContext";
-import { SeriesContextType } from "../../types/SeriesContextType";
+import { FC, ChangeEvent, useState, useEffect, useContext } from 'react';
+import { seriesService } from '../../services/seriesService';
+import { ISeries } from '../../interfaces/ISeries';
+import { Badge, Button, Col, FloatingLabel, Form, Row } from 'react-bootstrap';
+import { IGenre } from '../../interfaces/IGenre';
+import { IEpisode } from '../../interfaces/IEpisode';
+import { SeriesContext } from '../../contexts/SeriesContext';
+import { SeriesContextType } from '../../types/SeriesContextType';
+import { genres } from '../../data/genres';
 
 const CreateSeriesForm: FC = () => {
-  const [newGenreName, setNewGenreName] = useState<string>("");
-  const [newEpisodeName, setNewEpisodeName] = useState<string>("");
-  const [newSeasonNumber, setNewSeasonNumber] = useState<string>("");
-  const [newEpisodeNumber, setNewEpisodeNumber] = useState<string>("");
-  const [newGenre, setNewGenre] = useState<IGenre[]>([]);
-  const [newEpisode, setNewEpisode] = useState<IEpisode[]>([]);
+	const [newEpisodeName, setNewEpisodeName] = useState<string>('');
+	const [newSeasonNumber, setNewSeasonNumber] = useState<string>('');
+	const [newEpisodeNumber, setNewEpisodeNumber] = useState<string>('');
+	const [newGenre, setNewGenre] = useState<IGenre[]>([]);
+	const [newEpisode, setNewEpisode] = useState<IEpisode[]>([]);
 
-  const [newSeries, setNewSeries] = useState<ISeries>({
-    name: "",
-    image: "",
-    genre: newGenre,
-    plot: "",
-    episodes: newEpisode,
-  });
+	const [newSeries, setNewSeries] = useState<ISeries>({
+		name: '',
+		image: '',
+		genre: newGenre,
+		plot: '',
+		episodes: newEpisode,
+	});
 
-  const [newImage, setNewImage] = useState<File>();
+	const [newImage, setNewImage] = useState<File>();
 
-  const { saveSeries } = useContext(SeriesContext) as SeriesContextType;
+	const { saveSeries } = useContext(SeriesContext) as SeriesContextType;
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    let { name, value, checked } = event.target;
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		let { name, value, checked } = event.target;
 
-    switch (name) {
-      case "seriesName":
-        setNewSeries({ ...newSeries, name: value });
-        break;
-      case "image":
-        let { files } = event.target;
-        if (files) {
-          setNewSeries({ ...newSeries, image: files[0].name });
-          setNewImage(files[0]);
-        }
-        break;
-      case "Action":
-      case "Adventure":
-      case "Sci-Fi":
-      case "Comedy":
-      case "Romance":
-      case "Horror":
-      case "Thriller":
-      case "Drama":
-        if (checked) {
-          setNewGenreName(name);
-        }
-        break;
-      case "episodeName":
-        setNewEpisodeName(value);
-        break;
-      case "seasonNumber":
-        setNewSeasonNumber(value);
-        break;
-      case "episodeNumber":
-        setNewEpisodeNumber(value);
-        break;
-      case "plot":
-        setNewSeries({ ...newSeries, plot: value });
-        break;
-    }
-  };
+		switch (name) {
+			case 'seriesName':
+				setNewSeries({ ...newSeries, name: value });
+				break;
+			case 'image':
+				let { files } = event.target;
+				if (files) {
+					setNewSeries({ ...newSeries, image: files[0].name });
+					setNewImage(files[0]);
+				}
+				break;
+			case 'Action':
+			case 'Adventure':
+			case 'Sci-Fi':
+			case 'Comedy':
+			case 'Romance':
+			case 'Horror':
+			case 'Thriller':
+			case 'Drama':
+				if (checked) {
+					setNewGenre([...newGenre, { name }]);
+				} else if (!checked) {
+					const result = newGenre.filter(
+						({ name }) => name !== event.target.name
+					);
+					setNewGenre(result);
+				}
+				break;
+			case 'episodeName':
+				setNewEpisodeName(value);
+				break;
+			case 'seasonNumber':
+				setNewSeasonNumber(value);
+				break;
+			case 'episodeNumber':
+				setNewEpisodeNumber(value);
+				break;
+			case 'plot':
+				setNewSeries({ ...newSeries, plot: value });
+				break;
+		}
+	};
 
-  useEffect(() => {
-    setNewSeries({ ...newSeries, genre: newGenre });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newGenre]);
+	useEffect(() => {
+		setNewSeries({ ...newSeries, genre: newGenre });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [newGenre]);
 
-  const addNewEpisode = () => {
-    setNewEpisode([
-      ...newEpisode,
-      {
-        name: newEpisodeName,
-        seasonNumber: newSeasonNumber,
-        episodeNumber: newEpisodeNumber,
-      },
-    ]);
-    setNewEpisodeName("");
-    setNewSeasonNumber("");
-    setNewEpisodeNumber("");
-  };
+	const addNewEpisode = () => {
+		setNewEpisode([
+			...newEpisode,
+			{
+				name: newEpisodeName,
+				seasonNumber: newSeasonNumber,
+				episodeNumber: newEpisodeNumber,
+			},
+		]);
+		setNewEpisodeName('');
+		setNewSeasonNumber('');
+		setNewEpisodeNumber('');
+	};
 
-  useEffect(() => {
-    setNewSeries({ ...newSeries, episodes: newEpisode });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newEpisode]);
+	useEffect(() => {
+		setNewSeries({ ...newSeries, episodes: newEpisode });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [newEpisode]);
 
-  const listGenres = () => {
-    return newGenre.map((genre: IGenre, key: number) => {
-      return (
-        <Badge className="mx-1" bg="secondary" key={key}>
-          {genre.name}
-        </Badge>
-      );
-    });
-  };
+	const listGenres = () => {
+		return newGenre.map((genre: IGenre, key: number) => {
+			return (
+				<Badge className="mx-1" bg="secondary" key={key}>
+					{genre.name}
+				</Badge>
+			);
+		});
+	};
 
-  const listEpisodes = () => {
-    return newEpisode.map((episode: IEpisode, key: number) => {
-      return (
-        <Badge className="mx-1" bg="secondary" key={key}>
-          {`"${episode.name}" as episode ${episode.episodeNumber} in season ${episode.seasonNumber}`}
-        </Badge>
-      );
-    });
-  };
+	const listEpisodes = () => {
+		return newEpisode.map((episode: IEpisode, key: number) => {
+			return (
+				<Badge className="mx-1" bg="secondary" key={key}>
+					{`"${episode.name}" as episode ${episode.episodeNumber} in season ${episode.seasonNumber}`}
+				</Badge>
+			);
+		});
+	};
 
-  const postNewSeries = () => {
-    seriesService.postSeries(newSeries, newImage as File);
-    saveSeries(newSeries);
+	const postNewSeries = () => {
+		seriesService.postSeries(newSeries, newImage as File);
+		saveSeries(newSeries);
 
-    setNewSeries({
-      name: "",
-      image: "",
-      genre: [],
-      plot: "",
-      episodes: [],
-    });
-    setNewGenreName("");
-    setNewEpisodeName("");
-    setNewSeasonNumber("");
-    setNewEpisodeNumber("");
-    setNewGenre([]);
-    setNewEpisode([]);
-  };
+		setNewSeries({
+			name: '',
+			image: '',
+			genre: [],
+			plot: '',
+			episodes: [],
+		});
+		setNewEpisodeName('');
+		setNewSeasonNumber('');
+		setNewEpisodeNumber('');
+		setNewGenre([]);
+		setNewEpisode([]);
+	};
 
-  useEffect(() => {
-    setNewGenre([...newGenre, { name: newGenreName }]);
-    console.log("HERE " + newGenreName);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newGenreName]);
+	const listGenresCheckbox = () => {
+		return genres.map((genre, key) => {
+			return (
+				<Form.Check
+					inline
+					label={genre.name}
+					name={genre.name}
+					type="checkbox"
+					key={key}
+					onChange={handleChange}
+				/>
+			);
+		});
+	};
 
-  const genres = [
-    {
-      name: "Action",
-    },
-    {
-      name: "Adventure",
-    },
-    {
-      name: "Sci-Fi",
-    },
-    {
-      name: "Comedy",
-    },
-    {
-      name: "Romance",
-    },
-    {
-      name: "Horror",
-    },
-    {
-      name: "Thriller",
-    },
-    {
-      name: "Drama",
-    },
-  ];
+	return (
+		<>
+			{/* Series Name */}
+			<FloatingLabel
+				className="input-label"
+				controlId="seriesName"
+				label="Series name"
+			>
+				<Form.Control
+					onChange={handleChange}
+					name="seriesName"
+					type="text"
+					placeholder="What is the series called?"
+					value={newSeries.name}
+				/>
+			</FloatingLabel>
 
-  const listGenresCheckbox = () => {
-    return genres.map((genre, key) => {
-      return (
-        <Form.Check
-          inline
-          label={genre.name}
-          name={genre.name}
-          type="checkbox"
-          key={key}
-          onChange={handleChange}
-        />
-      );
-    });
-  };
+			{/* Image */}
+			<Form.Group controlId="seriesImage" className="mb-3 mt-3">
+				<Form.Label>Series image</Form.Label>
+				<Form.Control onChange={handleChange} name="image" type="file" />
+			</Form.Group>
 
-  return (
-    <>
-      {/* Series Name */}
-      <FloatingLabel
-        className="input-label"
-        controlId="seriesName"
-        label="Series name"
-      >
-        <Form.Control
-          onChange={handleChange}
-          name="seriesName"
-          type="text"
-          placeholder="What is the series called?"
-          value={newSeries.name}
-        />
-      </FloatingLabel>
+			{/* Plot */}
+			<FloatingLabel className="input-label" controlId="plot" label="Plot">
+				<Form.Control
+					onChange={handleChange}
+					className="mb-3"
+					as="textarea"
+					style={{ height: '100px' }}
+					name="plot"
+					type="text"
+					placeholder="Once upon a time..."
+					value={newSeries.plot}
+				/>
+			</FloatingLabel>
 
-      {/* Image */}
-      <Form.Group controlId="seriesImage" className="mb-3 mt-3">
-        <Form.Label>Series image</Form.Label>
-        <Form.Control onChange={handleChange} name="image" type="file" />
-      </Form.Group>
+			{/* Genre */}
+			<div className="mb-3">{listGenresCheckbox()}</div>
 
-      {/* Plot */}
-      <FloatingLabel className="input-label" controlId="plot" label="Plot">
-        <Form.Control
-          onChange={handleChange}
-          className="mb-3"
-          as="textarea"
-          style={{ height: "100px" }}
-          name="plot"
-          type="text"
-          placeholder="Once upon a time..."
-          value={newSeries.plot}
-        />
-      </FloatingLabel>
+			{/* List all genres added */}
+			<p className="mt-1">Genres added: {listGenres()}</p>
 
-      {/* Genre */}
-      <div className="mb-3">{listGenresCheckbox()}</div>
+			{/* Episodename, seasonnumber og episodenumber */}
+			<FloatingLabel
+				className="mb-3 input-label"
+				controlId="episodeName"
+				label="Episode name"
+			>
+				<Form.Control
+					onChange={handleChange}
+					name="episodeName"
+					type="text"
+					placeholder="Episode name"
+					value={newEpisodeName}
+				/>
+			</FloatingLabel>
+			<Row>
+				<Col>
+					<FloatingLabel
+						className="input-label"
+						controlId="seasonNumber"
+						label="Season number"
+					>
+						<Form.Control
+							onChange={handleChange}
+							name="seasonNumber"
+							type="text"
+							placeholder="Which season is this?"
+							value={newSeasonNumber}
+						/>
+					</FloatingLabel>
+				</Col>
+				<Col>
+					<FloatingLabel
+						className="input-label"
+						controlId="episodeNumber"
+						label="Episode number"
+					>
+						<Form.Control
+							onChange={handleChange}
+							name="episodeNumber"
+							type="text"
+							placeholder="Episode number?"
+							value={newEpisodeNumber}
+						/>
+					</FloatingLabel>
+				</Col>
+			</Row>
 
-      {/* List all genres added */}
-      <p className="mt-1">Genres added: {listGenres()}</p>
+			{/* Add new episode */}
+			{newEpisodeName && newSeasonNumber && newEpisodeNumber ? (
+				<Button
+					onClick={addNewEpisode}
+					className="my-3"
+					variant="secondary"
+					type="submit"
+				>
+					Add episode
+				</Button>
+			) : (
+				<Button className="my-3" variant="secondary" type="submit" disabled>
+					Add episode
+				</Button>
+			)}
 
-      {/* Episodename, seasonnumber og episodenumber */}
-      <FloatingLabel
-        className="mb-3 input-label"
-        controlId="episodeName"
-        label="Episode name"
-      >
-        <Form.Control
-          onChange={handleChange}
-          name="episodeName"
-          type="text"
-          placeholder="Episode name"
-          value={newEpisodeName}
-        />
-      </FloatingLabel>
-      <Row>
-        <Col>
-          <FloatingLabel
-            className="input-label"
-            controlId="seasonNumber"
-            label="Season number"
-          >
-            <Form.Control
-              onChange={handleChange}
-              name="seasonNumber"
-              type="text"
-              placeholder="Which season is this?"
-              value={newSeasonNumber}
-            />
-          </FloatingLabel>
-        </Col>
-        <Col>
-          <FloatingLabel
-            className="input-label"
-            controlId="episodeNumber"
-            label="Episode number"
-          >
-            <Form.Control
-              onChange={handleChange}
-              name="episodeNumber"
-              type="text"
-              placeholder="Episode number?"
-              value={newEpisodeNumber}
-            />
-          </FloatingLabel>
-        </Col>
-      </Row>
+			{/* List all episodes added */}
+			<p className="mt-1">Episodes added: {listEpisodes()}</p>
 
-      {/* Add new episode */}
-      {newEpisodeName && newSeasonNumber && newEpisodeNumber ? (
-        <Button
-          onClick={addNewEpisode}
-          className="my-3"
-          variant="secondary"
-          type="submit"
-        >
-          Add episode
-        </Button>
-      ) : (
-        <Button className="my-3" variant="secondary" type="submit" disabled>
-          Add episode
-        </Button>
-      )}
-
-      {/* List all episodes added */}
-      <p className="mt-1">Episodes added: {listEpisodes()}</p>
-
-      {/* Post new series */}
-      <Button
-        className="mt-3"
-        onClick={postNewSeries}
-        variant="primary"
-        type="submit"
-      >
-        Submit new series
-      </Button>
-    </>
-  );
+			{/* Post new series */}
+			<Button
+				className="mt-3"
+				onClick={postNewSeries}
+				variant="primary"
+				type="submit"
+			>
+				Submit new series
+			</Button>
+		</>
+	);
 };
 
 export default CreateSeriesForm;
